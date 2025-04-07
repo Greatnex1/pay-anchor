b6b8af9a0a270ba595b744523b8e4ba5bb2c02db
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-07 16:32:41 +0100

    add generic exception

---
+++ src/main/java/com/payu/payly/exception/GenericException.java
@@ -0,0 +1,19 @@
+package com.payu.payly.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class GenericException extends RuntimeException {
+
+    private final HttpStatus status;
+
+    public GenericException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
+
+    public static GenericException badRequest(String message) {
+        return new GenericException(message, HttpStatus.BAD_REQUEST);
+    }
+}
