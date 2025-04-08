2c4186852795d6de8f3c0d8de7a1447ae7e204d4
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-08 17:17:19 +0100

    add product detail

--- src/main/java/com/payu/payly/model/ProductDetail.java
+++ src/main/java/com/payu/payly/model/ProductDetail.java
@@ -1,5 +1,6 @@
 package com.payu.payly.model;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
@@ -8,16 +9,16 @@ import lombok.*;
 
 import java.math.BigDecimal;
 
-@Setter
-@Getter
-@ToString
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDetail {
     @NotEmpty
+    @Column(length = 500)
     private String productName;
-
+    @Positive
     private BigDecimal productPrice;
     @Positive
     private int productQuantity;
+
 }
