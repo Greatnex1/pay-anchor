7f43ffd439f0ed4be5097877dcf21d739567112a
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-08 17:17:49 +0100

    add error messages

---
+++ src/main/java/com/payu/payly/constants/ErrorMessages.java
@@ -0,0 +1,8 @@
+package com.payu.payly.constants;
+
+public class ErrorMessages {
+    public static final String MERCHANT_NOT_FOUND = "Merchant not found";
+    public static final String INVOICE_NOT_FOUND = "Invoice not found";
+    public static final String EMPTY_INPUT_ERROR = " %s Field cannot be empty. Please provide an input";
+
+}
