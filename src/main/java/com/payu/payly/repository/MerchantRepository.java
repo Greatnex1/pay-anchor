ea99f84f08cb59f9d1e2bda03c52f5c7627a5595
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-08 13:12:48 +0100

    add findByEmail in merchant repository

--- src/main/java/com/payu/payly/repository/MerchantRepository.java
+++ src/main/java/com/payu/payly/repository/MerchantRepository.java
@@ -3,5 +3,8 @@ package com.payu.payly.repository;
 import com.payu.payly.model.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
+    Optional<Merchant> findByEmail(String email);
 }
