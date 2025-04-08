f7c4d9e4f81baa75f6e6cf3bed25a5d9ac843072
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-08 18:10:11 +0100

    updated error message

--- src/main/java/com/payu/payly/constants/ErrorMessages.java
+++ src/main/java/com/payu/payly/constants/ErrorMessages.java
@@ -4,5 +4,6 @@ public class ErrorMessages {
     public static final String MERCHANT_NOT_FOUND = "Merchant not found";
     public static final String INVOICE_NOT_FOUND = "Invoice not found";
     public static final String EMPTY_INPUT_ERROR = " %s Field cannot be empty. Please provide an input";
+    public static final String UNDEFINED = "UNDEFINED";
 
 }
