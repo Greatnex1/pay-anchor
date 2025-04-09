ef160cea12feb1f350f73781b5a670b623be6ebc
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-09 13:31:38 +0100

    add payment properties config

--- src/main/java/com/payu/payly/PaylyApplication.java
+++ src/main/java/com/payu/payly/PaylyApplication.java
@@ -1,13 +1,15 @@
 package com.payu.payly;
 
+import com.payu.payly.config.PaymentProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableCaching
+@EnableConfigurationProperties(PaymentProperties.class)
 @Slf4j
 public class PaylyApplication {
 
--- src/main/java/com/payu/payly/config/PaymentProperties.java
+++ src/main/java/com/payu/payly/config/PaymentProperties.java
@@ -2,16 +2,17 @@ package com.payu.payly.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 @Setter
+@ConfigurationProperties(prefix = "paystack")
 public class PaymentProperties {
-      @Value("${invoice.redirect.url}")
+//      @Value("${invoice.redirect.url}")
     private String invoiceRedirectUrl;
-        @Value("${paystack.url}")
+      @Value("${paystack.url}")
     private String paystackUrl;
-       @Value("${paystack.secret.key}")
+      @Value("${paystack.paystack-secret-key}")
     private String paystackSecretKey;
 }
